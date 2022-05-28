c4df6b355464a4084a8601c29c113f679cf745db
Mixxmaster <dundukovmi@gmail.com>
2022-05-28 18:21:39 +0300

    add Search-bar focus option after clear

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
 const Search = () => {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
 
   return (
     <div className={styles.root}>
@@ -12,6 +18,7 @@ const Search = () => {
         <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         className={styles.input}
@@ -19,7 +26,7 @@ const Search = () => {
         type="text"
       />
       {searchValue && (
-        <svg onClick={() => setSearchValue('')} className={styles.clear} viewBox="0 0 200 200">
+        <svg onClick={() => onClickClear()} className={styles.clear} viewBox="0 0 200 200">
           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
         </svg>
       )}
