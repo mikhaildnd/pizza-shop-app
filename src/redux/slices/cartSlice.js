af5aef93adfd9e08ec6e1166d607cc2feea42fea
Mixxmaster <dundukovmi@gmail.com>
2022-06-05 22:59:17 +0300

    add Cart state slice

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const duplicateItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (duplicateItem) {
+        duplicateItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+    },
+    clearItems(state) {
+      state.items = [];
+      state.totalPrice = 0;
+    },
+    minusItem(state, action) {
+      const duplicateItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (duplicateItem && duplicateItem.count > 1) {
+        duplicateItem.count--;
+      }
+    },
+  },
+});
+
+export const { addItem, minusItem, removeItem, clearItems } = cartSlice.actions;
+
+export default cartSlice.reducer;
