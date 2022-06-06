6e2cb756fc36c1ca740993b58aa76e7e59f9bee1
Mixxmaster <dundukovmi@gmail.com>
2022-06-07 01:26:53 +0300

    add request logic (thunk and extra reducer)

--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -1,7 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchPizzas = createAsyncThunk('pizzas/fetchPizzasStatus', async (params) => {
+  const { category, sortBy, order, search, currentPage, limitProductsOnPage } = params;
+
+  const address = `https://628bd2d1667aea3a3e36d84e.mockapi.io/products`;
+  const requestParams = `?page=${currentPage}&limit=${limitProductsOnPage}&${category}&sortBy=${sortBy}&order=${order}&search=${search}`;
+  const requestLink = `${address}${requestParams}`;
+
+  const { data } = await axios.get(requestLink);
+
+  return data;
+});
 
 const initialState = {
   products: [],
+  status: 'loading',
 };
 
 const pizzasSlice = createSlice({
@@ -10,6 +24,20 @@ const pizzasSlice = createSlice({
   reducers: {
     setProducts(state, action) {
       state.products = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchPizzas.pending]: (state) => {
+      state.status = 'loading';
+      state.products = [];
+    },
+    [fetchPizzas.fulfilled]: (state, action) => {
+      state.status = 'success';
+      state.products = action.payload;
+    },
+    [fetchPizzas.rejected]: (state) => {
+      state.status = 'error';
+      state.products = [];
     },
   },
 });
