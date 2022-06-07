d765d4e777ac3d823a2e52b2a479c244a95d6d2e
Mixxmaster <dundukovmi@gmail.com>
2022-06-07 21:47:33 +0300

    wrapped block in a link

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { addItem, selectCartItemById } from '../../redux/slices/cartSlice';
 
 const typeNames = ['тонкое', 'традиционное'];
@@ -28,7 +29,11 @@ const PizzaBlock = ({ id, title, price, imageUrl, sizes, types }) => {
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-      <h4 className="pizza-block__title">{title}</h4>
+      <h4 className="pizza-block__title">
+        <Link className="pizza-block__link" to={`/pizza/${id}`}>
+          {title}
+        </Link>
+      </h4>
       <div className="pizza-block__selector">
         <ul>
           {types.map((typeIdx, idx) => (
