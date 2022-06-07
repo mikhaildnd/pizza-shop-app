51206d530ee8795ea5060353b3a0424941c75ad0
Mixxmaster <dundukovmi@gmail.com>
2022-06-07 21:44:44 +0300

    minor upd address ink

--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const fetchPizzas = createAsyncThunk('pizzas/fetchPizzasStatus', async (params) => {
   const { category, sortBy, order, search, currentPage, limitProductsOnPage } = params;
 
-  const address = `https://628bd2d1667aea3a3e36d84e.mockapi.io/products`;
+  const address = `https://628bd2d1667aea3a3e36d84e.mockapi.io/products/`;
   const requestParams = `?page=${currentPage}&limit=${limitProductsOnPage}&${category}&sortBy=${sortBy}&order=${order}&search=${search}`;
   const requestLink = `${address}${requestParams}`;
 
