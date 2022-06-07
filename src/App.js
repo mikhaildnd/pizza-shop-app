dc463f287da7b96c14b566790757998c6435eaa0
Mixxmaster <dundukovmi@gmail.com>
2022-06-07 21:48:50 +0300

    add fullPizza route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
+import FullPizza from './pages/FullPizza';
 
 import './scss/app.scss';
 
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/pizza/:id" element={<FullPizza />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
